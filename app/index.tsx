00607bede448bd21218e72bed92101b4d2f17214
Josef Bauer <josef.bauer.1st@gmail.com>
2025-05-29 21:26:11 +0200

    Reset project

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{
+        color: "white",
+        backgroundColor: "purple",
+        width: "100%",
+        textAlign: 'center'
+      }}>jaja nene</Text>
+    </View>
+  );
+}
